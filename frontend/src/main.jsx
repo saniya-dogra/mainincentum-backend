aedcec0992e473db0e7eb95c861727552d6c7359
Harshit Saini <harshitsaini0019@gmail.com>
2024-12-15 01:45:02 +0530

    update loginffg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'; 
+import AllInOneChat from './components/widget/allinonechat.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
   <ScrollToTop />
+ <AllInOneChat />
   <App />
   </BrowserRouter>
 </StrictMode>,
