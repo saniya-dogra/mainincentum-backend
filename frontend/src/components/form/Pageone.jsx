31d50da1348f67bb0c0596fd8e2acac11b1942fd
codewithprince0 <codewithprince0@gmail.com>
2024-12-16 17:28:35 +0530

    commiit

--- frontend/src/components/form/Pageone.jsx
+++ frontend/src/components/form/Pageone.jsx
@@ -166,12 +166,6 @@ export default function PageOne() {
           <ApplicantSection />
         </div>
       </div>
-      <div className="flex justify-end max-w-full w-[12%] m-auto mt-5 mb-5 " style={{ marginLeft: "60%" }}>
-      <Link to = "/vehicle-details-VehicleTwo"
-       className="text-lg font-medium px-6 py-2 text-center text-white bg-blue-500 rounded-xl hover:bg-blue-600 transition cursor-pointer w-full">
-          Next
-          </Link>
-      </div>
     </div>
   );
 }
