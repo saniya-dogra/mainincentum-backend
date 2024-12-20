b2f5dfd115c3e729591f623ef0d25651d58b1b03
Harshit Saini <harshitsaini0019@gmail.com>
2024-12-20 12:34:24 +0530

    update twentytwelve

--- frontend/src/Pages/loans/vehicleLoan/VehicleLoan.jsx
+++ frontend/src/Pages/loans/vehicleLoan/VehicleLoan.jsx
@@ -21,7 +21,7 @@ export default function VehicelLoan() {
             </Link>
           </button>
           <h2 className="font-bold text-[26px] sm:text-[48px] lg:text-[50px] leading-[44px] sm:leading-[60px] lg:leading-[55px]">
-            Drive The Car You've Always Dreamed Of, With Ease
+          Drive The Car You've Always Dreamed Of, With Ease
           </h2>
           <div className="flex lg:hidden justify-center items-center mt-12 lg:mt-[310px]">
             <img
@@ -31,18 +31,15 @@ export default function VehicelLoan() {
             />
           </div>
           <p className="text-md sm:text-lg lg:text-[14px] leading-[20px] sm:leading-[25px] lg:leading-[29px] text-gray-800 mt-4 lg:mt-3">
-           We know owning you dream car isn't about the ride, it's about the freedom and joy that comes with is. Our AI driven consultantion is here to make that happen, without the stress. Whether you're eyeing a aleek sports car, a family-friendly SUV, or a reliable everyday vehicle, we,ve got you covered.{" "}
-          </p>
+          We know owning you dream car isn't about the ride, it's about the freedom and joy that comes with is. Our AI driven consultantion is here to make that happen, without the stress. Whether you're eyeing a aleek sports car, a family-friendly SUV, or a reliable everyday vehicle, we,ve got you covered. </p>
    
           <p className="text-md sm:text-lg lg:text-[14px]  leading-[20px] sm:leading-[25px] lg:leading-[29px] text-gray-800">
-            We bring you any and every feature of car loan being offered by indian financial institution. Be it flexible repayment options, competitive rate, and quick approval, we take care of the complicated stuff so you can focus on what matters most-enjoying the road ahead.
-          </p>
+          We bring you any and every feature of car loan being offered by indian financial institution. Be it flexible repayment options, competitive rate, and quick approval, we take care of the complicated stuff so you can focus on what matters most-enjoying the road ahead.</p>
           <h2 className="text-xl  lg:text-[22px]  mt-4 lg:mt-6 lg:leading-7 ">
-            And yes, we will incentivise you too.
+          And yes, we will incentivise you too.      
           </h2>
           <h2 className="text-xl lg:text-[20px] lg:leading-7  ">
-            You are a click away from your dream car. Are you ready for the
-            drive of your dreams?
+          You are a click away from your dream car. Are you ready for the drive of your dreams?
           </h2>
           <Link to={'/vehicle-details-Vehicleone'}>
           <Button/>
